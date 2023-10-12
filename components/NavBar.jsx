cd164c5707b099b5fc75ad78d9446944af067280
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-10-12 12:09:01 +0300

    navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,54 +4,52 @@ function NavBar({ user }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/players">
-                Players
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/games">
-                Games
-              </a>
-            </li>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/players">
+              Players
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/games">
+              Games
+            </a>
+          </li>
 
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <a className="nav-link" href="/favorites">
-                    Favorites
-                  </a>
-                </li>
-                <li>Hello, {user.email}!</li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/api/auth/logout">
-                    LogOut
-                  </a>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <a className="nav-link" href="/auth/sign-up">
-                    Sign-Up
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/auth/sign-in">
-                    Sign-In
-                  </a>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
+          {user ? (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="/favorites">
+                  Favorites
+                </a>
+              </li>
+              <li>Hello, {user.email}!</li>
+              <li className="nav-item">
+                <a className="nav-link" href="/api/auth/logout">
+                  LogOut
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="/auth/sign-up">
+                  Sign-Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/auth/sign-in">
+                  Sign-In
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </nav>
   );
